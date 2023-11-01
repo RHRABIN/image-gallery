88a52f8a7fd9235db84eff3698197d4b9e24c026
RH Rabin <rhrabin934602@gmail.com>
2023-11-01 09:59:54 +0600

    header desined

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
 
 function App() {
+  const selectedFile = [1, 2];
   return (
-    <>
-      hello
-    </>
+    <main className='bg-white rounded-lg h-[40vh]'>
+      {/* title */}
+      <div className='flex justify-between border-b py-4 border-[#ccc]  px-4'>
+        {/* <h1 className="text-xl font-bold">Gallery</h1> */}
+        <h1 className="text-xl font-bold">
+          {selectedFile.length > 1 ? "Files" : "File"} selected
+        </h1>
+        <button className='text-red-600 font-semibold hover:border-b border-red-600'>Delete files</button>
+      </div>
+      <div className='p-4'>
+        main
+      </div>
+    </main>
   )
 }
 
